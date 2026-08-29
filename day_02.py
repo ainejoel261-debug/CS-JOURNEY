12b853491aa6b03921a20c4e9fc7f207b97c6dd1
ainejoel261-debug <ainejoel261@gmail.com>
2026-08-29 13:36:26 +0300

    Create day_02.py

---
+++ day_02.py
@@ -0,0 +1,44 @@
+name = "Joel Ainebyona"
+age = 21
+course = "Computer Science"
+is_student = True
+gpa = 3.75
+
+print(name)
+print(age)
+print(course)
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
+
+name_s = input("What is your name?")
+print("Hello " ,name_s)
+gpa = float(input("Enter your GPA: "))
+print("Your GPA is: ",gpa)
+
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+
+print("======= STUDENT INFORMATION =======")
+
+name = str(input("What is your name? "))
+age = int(input("What is your age? "))
+course = str(input("What is your course? "))
+cgpa = float(input("What is your cgpa? "))
+
+print("Name:" ,name)
+print("Age:" ,age)
+print("Course:" ,course)
+print("CGPA:" ,cgpa)
+
+print("===================================")

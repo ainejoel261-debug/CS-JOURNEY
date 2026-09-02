b0202564b9c8afdc7f6a18c92216c70afed8abe4
ainejoel261-debug <ainejoel261@gmail.com>
2026-09-02 17:04:55 +0300

    Update day_09.py

--- day_09.py
+++ day_09.py
@@ -26,3 +26,30 @@ while True:
     finally:
         print("CGPA validation finished.")
 
+
+from student import Student
+students = []
+for i in range(3):
+    name = input("Enter student name: ")
+    course = input("Enter your course: ")
+    while True:
+        try:
+            age = int(input("Enter student age: "))
+            break
+        except ValueError:
+            print("Please enter a valid age.")
+    while True:
+        try:
+            cgpa = float(input("Enter your CGPA: "))
+            if cgpa > 5 or cgpa < 0:
+                raise ValueError("CGPA is 0, 5 or between 0 and 5!")
+            break
+        except ValueError:
+            print("Enter the correct value CGPA!")
+    student = Student(name, age, course, cgpa)
+    students.append(student)
+for student in students:
+    student.display_info()
+    print(student.calculate_grade())
+
+
